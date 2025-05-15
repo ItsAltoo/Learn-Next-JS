e816725d64bc19e044233ebbd0f665607420712d
ItsAltoo <Itsmalik57@gmail.com>
2025-05-15 16:31:09 +0800

    15.5.25

--- next1/src/app/layout.tsx
+++ next1/src/app/layout.tsx
@@ -1,11 +1,7 @@
 import "./globals.css";
 import "./font.css"
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({children}: Readonly<{children: React.ReactNode;}>) {
   return (
     <html lang="en">
       <body>{children}</body>
--- next5/src/app/page.tsx
+++ next5/src/app/page.tsx
@@ -110,6 +110,7 @@ export default function Home() {
           >
             Delete
           </Button>
+
         </div>
           </div>
         ))}
